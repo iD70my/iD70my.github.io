779a8df1cd211b47522d48f67c951edef38d4e13
iD70my <l5_5@hotmail.com>
2015-07-25 14:25:11 +0300

    up

--- template/index/head2.php
+++ template/index/head2.php
@@ -2,6 +2,6 @@
 
 <html>
 <head>
-    <link href="test/css/ios7.css" rel="stylesheet">
+    <link href="css/ios7.css" rel="stylesheet">
     <meta content="width=device-width, user-scalable=no" name="viewport">
     <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
