8b109a122619c7a8ade924a416794cb621ce7540
iD70my <l5_5@hotmail.com>
2015-07-25 15:19:09 +0300

    up

--- template/index/devLink.php
+++
@@ -1,4 +0,0 @@
-<ul>
-    <li>
-        <a class=\"devLink\" target=\"_blank\" href="https://twitter.com/YourTwitter">@ADeveloper on Twitter</a></li>
-    <li>
--- template/index/footer.php
+++
@@ -1,3 +0,0 @@
-<p><a href="http://rcrepo.com" target="_blank">Reddit Community Repo</a></p>
-</body>
-</html>
