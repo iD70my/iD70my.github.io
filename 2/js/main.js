6e3009dfc16b46fe386e84c07d81b6d0eeba2496
iD70my <l5_5@hotmail.com>
2015-10-11 17:52:33 +0300

    Update main.js

--- 2/js/main.js
+++ 2/js/main.js
@@ -16,7 +16,21 @@ function animateHeadline($headlines) {
 		setTimeout(function(){ hideWord( headline.find('.is-visible') ) }, animationDelay);
 		//other checks here ...
 	});
-}	
+}
+function hideWord($word) {
+	var nextWord = takeNext($word);
+	switchWord($word, nextWord);
+	setTimeout(function(){ hideWord(nextWord) }, animationDelay);
+}
+ 
+function takeNext($word) {
+	return (!$word.is(':last-child')) ? $word.next() : $word.parent().children().eq(0);
+}
+ 
+function switchWord($oldWord, $newWord) {
+	$oldWord.removeClass('is-visible').addClass('is-hidden');
+	$newWord.removeClass('is-hidden').addClass('is-visible');
+}
 	/*
 تجربة	
 	*/
