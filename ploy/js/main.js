5be62be2efdc67dbd9acab2fb62bf9a80e17896b
iD70my <o@outlook.kr>
2015-12-19 16:19:53 +0300

    up

--- ploy/js/main.js
+++ ploy/js/main.js
@@ -3,6 +3,38 @@ jQuery(document).ready(function(){
 		coverLayer = $('.cd-cover-layer');
 	
 	/*
+تجربة	
+	*/
+var animationDelay = 2500;
+ 
+animateHeadline($('.cd-headline'));
+ 
+function animateHeadline($headlines) {
+	$headlines.each(function(){
+		var headline = $(this);
+		//trigger animation
+		setTimeout(function(){ hideWord( headline.find('.is-visible') ) }, animationDelay);
+		//other checks here ...
+	});
+}
+function hideWord($word) {
+	var nextWord = takeNext($word);
+	switchWord($word, nextWord);
+	setTimeout(function(){ hideWord(nextWord) }, animationDelay);
+}
+ 
+function takeNext($word) {
+	return (!$word.is(':last-child')) ? $word.next() : $word.parent().children().eq(0);
+}
+ 
+function switchWord($oldWord, $newWord) {
+	$oldWord.removeClass('is-visible').addClass('is-hidden');
+	$newWord.removeClass('is-hidden').addClass('is-visible');
+}
+	/*
+تجربة	
+	*/
+	/*
 		convert a cubic bezier value to a custom mina easing
 		http://stackoverflow.com/questions/25265197/how-to-convert-a-cubic-bezier-value-to-a-custom-mina-easing-snap-svg
 	*/
